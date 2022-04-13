47aa9b44db7e06fd12b2c98210c8786df9baf22d
Travis <tsahadeo@mac.com>
2022-04-12 22:23:17 -0400

    commit tree d652ce7a413bab3d61ba604102884c45c60e285f

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
+  const seenNumbers = {};
+  console.log(seenNumbers)
+  for (const number of array) {
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+    return false
+  }
   // Write your algorithm here
-}
 
 /* 
   Write the Big O time complexity of your function here
